8068d2e91d5c079c5543f35779356e8ea54f1406
Dumssss <clementdumas11@gmail.com>
2024-04-04 10:57:22 +0200

    deleted some files

--- tp1-vagrant/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dnf update -y
-
-# Install de vim et python3 pour la partie 2
-dnf install -y vim python3
-
-# Install et activation de cloud init pour la partie 5
-dnf install -y cloud-init
-systemctl enable cloud-init
